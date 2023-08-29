ab8b2aa7180fff4451421398fb136457b2e2d93f
vikornienko <vikornienko76@gmail.com>
2023-08-29 21:38:19 +0500

    Fifth commit Add post-list and post-detail views. Add httpie library. Add http file with GET request.

--- app_posts/views.py
+++ app_posts/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 
 from .models import Post
 from .serializers import PostSerializer
@@ -15,3 +19,43 @@ class PostView(APIView):
         serializer = PostSerializer(queryset, many=True)
         # if serializer.is_valid():
         return Response(serializer.data)
+
+
+@csrf_exempt
+def post_list(request):
+    if request.method == 'GET':
+        queryset = Post.objects.all()
+        serializer = PostSerializer(queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        data = JSONParser.parse(request)
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def post_detail(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser.parse(request)
+        serializer = PostSerializer(post, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse(status=204)
--- drfBegin/urls.py
+++ drfBegin/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from app_posts.views import PostView
+from app_posts.views import PostView, post_list, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/posts/', PostView.as_view(), name='post-list')
+    path('api/posts/', PostView.as_view(), name='post-list'),
+    path('api/post-list/', post_list, name='post-list'),
+    path('api/posts/<int:pk>/', post_detail, name='post-detail'),
+    
 ]
