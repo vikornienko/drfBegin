1cc5f120e3c26cc3884fae6def5413f59c68fd1f
vikornienko <vikornienko76@gmail.com>
2023-08-29 08:35:49 +0500

    Third commit Create serializer, view and route for Post model. Reformat code after static analized.

--- drfBegin/urls.py
+++ drfBegin/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from app_posts.views import PostView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/posts/', PostView.as_view(), name='post-list')
 ]
